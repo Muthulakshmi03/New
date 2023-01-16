426eab87c776a8739db8b60322ffd07e93368fd2
Muthulakshmi03 <muthulakshmi03@gmail.com>
2023-01-16 13:18:19 +0530

    New Lines of code added

---
+++ SampleTest1.java
@@ -0,0 +1,19 @@
+package modifier1;
+
+public class SampleTest1 {
+
+	public static void main(String[] args) {
+		
+		add();
+	}
+	
+	private static void add() {
+		
+		int i=1;
+		int j=2;
+		int k=i+j;
+		System.out.println(k);
+		System.out.println("add function");
+	}
+	
+}
---
+++ SampleTest2.java
@@ -0,0 +1,13 @@
+package modifier1;
+
+public class SampleTest2 extends SampleTest1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		add();
+		
+		
+	}
+
+}
